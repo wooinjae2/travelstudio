9de1e32667fb0d975744c42b5c74881123022a5d
Kim MinSeop <kmsp91@gmail.com>
2017-08-07 19:49:53 +0900

    마이페이지 유저세팅
    ㄱㄱ

--- src/main/webapp/mypage/mypage.js
+++ src/main/webapp/mypage/mypage.js
@@ -1,39 +1,26 @@
 var userDesc= $('.user_desc');
 
 
-$.getJSON('../member/info.json', function(result) {
+$.getJSON('../member/header.json', function(result) {
 
-   console.log(result.data);
+   console.log(result);
        var template = Handlebars.compile($('#user-info-template').html())
-       var generatedHTML = template(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
+       var generatedHTML = template(result) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
 //       tbody.text('') // tbody의 기존 tr 태그들을 지우고
        userDesc.append(generatedHTML);
        
    $(document.body).on('click', '#mysetting', function(event) {
-	   location.href = 'user_setting.html?no=' + $(this).attr('data-no') 
+	   location.href = 'user_setting.html'
 	   event.preventDefault()
 	 })
+	 
+	 let str = result.memberPhoto;
+    if(str == null ) {
+      $('#user-img').attr('src', './usercircle.png').css('width', '170px').css('height', '170px').css('border-radius', '100%')
+    } else {
+	  $('#user-img').attr('src', '../upload/' + str).css('width', '170px').css('height', '170px').css('border-radius', '100%')
+    }
+	
 //       generatedHTML.appendTo($('.user_desc')).insertAfter('#mysetting');
 //.insertAfter('.')
 })
-//$.getJSON('../profile/info.json', function(result){
-//   console.log(result)
-//   let photo = result.data.memberPhoto
-//   console.log(result.data.info.memberPhoto)
-///*   $('<img>').css('width','200px').attr('src','upload/'+photo+'g.jp').appendTo('#definition')
-//  console.log($('img'))    */
-//})
-//var no = 0
-//no = $('#mysetting')
-//console.log(no)
-
-//$('#imgchecker').on('click',function(event){
-//    $.getJSON('dymember/detail.json', {'no':no}, function(result){
-//       console.log(result)
-//       let str = result.data.userPic
-//       console.log(str)
-//       $('<img>').css('width','200px').attr('src','upload/thumbnail/'+str+'_80.jpg').appendTo('#definition')
-//      console.log($('img'))    
-//    })
-
-
