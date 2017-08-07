df86c164841a70c479cdd56fe387213d4bad5e58
wooinjae2 <monsterfairy2@gmail.com>
2017-08-07 19:56:04 +0900

    .

--- src/main/webapp/jinseoKing/main.js
+++ src/main/webapp/jinseoKing/main.js
@@ -1,29 +1,27 @@
-var tbody = $('.box_slide_main');
 
 $.getJSON('/travelstudio/post/list.json', function(result) {
 
    console.log(result.data.list);
-       var template = Handlebars.compile($('#content-template').html())
+       var template = Handlebars.compile($('#content-template1').html())
        var generatedHTML = template(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
 //       tbody.text('') // tbody의 기존 tr 태그들을 지우고
-       tbody.append(generatedHTML) // 새 tr 태그들로 설정한다.
+       $('.box_slide_main').append(generatedHTML) // 새 tr 태그들로 설정한다.
 
   }) // getJSON()ile(title)
   
   
-  var mainbox = $('.owl-stage');
+/*  var mainbox = $('.owl-stage');*/
 
 $.getJSON('/travelstudio/member/info.json', function(result) {
 
    console.log(result.data.info);
-       var template2 = Handlebars.compile($('#content-template-2').html())
+       var template2 = Handlebars.compile($('#content-template4').html())
        var generatedHTML2 = template2(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
 //       tbody.text('') // tbody의 기존 tr 태그들을 지우고
        mainbox.append(generatedHTML2) // 새 tr 태그들로 설정한다.
 
   }) // getJSON()ile(title)
   
-  var tbody = $('.slide_bar_content');
 
 $.getJSON('/travelstudio/member/header.json', function(result) {
 
@@ -32,11 +30,11 @@ $.getJSON('/travelstudio/member/header.json', function(result) {
 	    var template = Handlebars.compile($('#tbody-template4').html())
 	    var generatedHTML = template(result) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
 //	    tbody.text('') // tbody의 기존 tr 태그들을 지우고
-	    tbody.append(generatedHTML) // 새 tr 태그들로 설정한다.
+	    $('.slide_bar_content').append(generatedHTML) // 새 tr 태그들로 설정한다.
 	    
 	    
 	    console.log(mno);
-	      $.post('../post/count.json',
+	      $.post('/post/count.json',
 	    		  {mno : mno}	
 	      , function(result) {
 	    	  console.log(result.data.list.length)
@@ -50,4 +48,4 @@ $.getJSON('/travelstudio/member/header.json', function(result) {
   })
 
   }) // getJSON()
-  
+
