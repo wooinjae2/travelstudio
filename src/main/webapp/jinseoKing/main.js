e4f1b13a6f34f723bbe53c94916d45de62b2a650
Kjinseo <kinbb1002@hanmail.net>
2017-08-28 20:24:02 +0900

    .

--- src/main/webapp/jinseoKing/main.js
+++ src/main/webapp/jinseoKing/main.js
@@ -1,3 +1,5 @@
+var mno;
+var numOfPost;
 $.getJSON('/post/list.json', function(result) {
 
    console.log(result.data.list);
@@ -41,6 +43,7 @@ $.getJSON('/member/header.json', function(result) {
 		$('#start-my-journey').click(function(){
 		   location.href="./login.html"
 		    //Other code etc.
+		    selectLoginUserPost()
 		});
 	}
 	    var template = Handlebars.compile($('#tbody-template4').html())
@@ -60,7 +63,86 @@ $.getJSON('/member/header.json', function(result) {
 //	    tbody.text('') // tbody의 기존 tr 태그들을 지우고
 	    generatedHTML='';
 	    $('.counting1').html(result.data.list.length) // 새 tr 태그들로 설정한다.
-
+   address()
+   selectLoginUserPost()
   })
-
+   
   }) // getJSON()
+  
+ 
+
+  
+  function selectLoginUserPost(){
+	console.log(mno)
+$.post('../post/selectOneUserPost.json',{'number':mno}, function(result) {
+	  console.log(result);
+	  for(i=0; i<=result.data.selectOneUserPost.length; i++){
+	  console.log(result.data.selectOneUserPost.length);
+	  numOfPost = result.data.selectOneUserPost.length;
+	  console.log(numOfPost);
+	  }
+     
+  $('<input id="numOfPost">').attr('value',numOfPost).attr("readonly",true).attr("disabled",false).appendTo($('.counting1'))
+})
+}
+
+function address(){
+	console.log(mno)
+	$.post('../detail/selectAddress.json', {
+		'mno': mno
+	},
+	function(result) {
+		console.log(result)
+		var uniqueNames = [];
+		var flag_list=[];
+		/*var flag_list_show=new Array();*/
+		var flag_count=0;
+
+		for(i=0;i<result.data.selectAddress.length;i++){
+			if(result.data.selectAddress[i]!=null){
+			if(result.data.selectAddress[i].address!=undefined){
+				flag_list[flag_count++]=result.data.selectAddress[i].address
+				console.log(flag_list)
+				
+			}
+			}
+		}
+		for(i=0;i<flag_list.length;i++){
+			console.log(flag_list[i].indexOf("대한민국"))
+			if(flag_list[i]!=undefined){
+				if(flag_list[i].indexOf("대한민국")!=-1){
+					flag_list[i] ='./flags/png/south-korea.png'
+				}else if(flag_list[i].indexOf("미국")!=-1){
+					flag_list[i]='./flags/png/united-states-of-america.png'
+				}else if(flag_list[i].indexOf("일본")!=-1){
+					flag_list[i]='./flags/png/japan.png'
+				}else if(flag_list[i].indexOf("영국")!=-1){
+					flag_list[i]='./flags/png/united-kingdom.png'
+				}else if(flag_list[i].indexOf("프랑스")!=-1){
+					flag_list[i]='./flags/png/france.png'
+				}else if(flag_list[i].indexOf("중국")!=-1){
+					flag_list[i]='./flags/png/china.png'
+				}else if(flag_list[i].indexOf("조선")!=-1){
+					flag_list[i]='./flags/png/north-korea.png'
+				}else if(flag_list[i].indexOf("스페인")!=-1){
+					flag_list[i]='./flags/png/spain.png'
+				}
+			}
+
+		}
+		
+		
+		$.each(flag_list, function(i, el){
+			if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+		});
+		console.log(uniqueNames)
+		console.log(uniqueNames.length)
+		numOfFlag=uniqueNames.length;
+		console.log(numOfFlag);
+		for(i=0;i<=uniqueNames.length;i++){
+			$('<img style=width:36px; height:36px;>').attr('src',uniqueNames[i]).css('margin-right','7px').appendTo($('.user_nation'))
+		}
+		
+		$('<input id="numberOfFlag">').attr('value',numOfFlag).attr("readonly",true).attr("disabled",false).appendTo($('.counting2'))
+		})
+	}
